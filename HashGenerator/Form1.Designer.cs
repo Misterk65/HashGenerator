8a906055402611afcf55b82fa91ed39d516c7fd0
Karsten Peucker <karsten.peucker@icloud.com>
2019-01-14 17:52:57 +0900

    Initial Version

--- HashGenerator/Form1.Designer.cs
+++ HashGenerator/Form1.Designer.cs
@@ -29,12 +29,202 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtMd5 = new System.Windows.Forms.TextBox();
+            this.lblMd5 = new System.Windows.Forms.Label();
+            this.txtSha256 = new System.Windows.Forms.TextBox();
+            this.lblSha256 = new System.Windows.Forms.Label();
+            this.btnCopyToClipMd5 = new System.Windows.Forms.Button();
+            this.btnCopyToClipSha = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusfilePath = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelVer = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.AutoSize = true;
+            this.btnOpenFile.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnOpenFile.Location = new System.Drawing.Point(448, 9);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(26, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(480, 9);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(12, 9);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(23, 13);
+            this.lblFile.TabIndex = 2;
+            this.lblFile.Text = "File";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(66, 9);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(376, 20);
+            this.txtFile.TabIndex = 3;
+            // 
+            // txtMd5
+            // 
+            this.txtMd5.Location = new System.Drawing.Point(66, 35);
+            this.txtMd5.Name = "txtMd5";
+            this.txtMd5.Size = new System.Drawing.Size(376, 20);
+            this.txtMd5.TabIndex = 6;
+            // 
+            // lblMd5
+            // 
+            this.lblMd5.AutoSize = true;
+            this.lblMd5.Location = new System.Drawing.Point(12, 35);
+            this.lblMd5.Name = "lblMd5";
+            this.lblMd5.Size = new System.Drawing.Size(30, 13);
+            this.lblMd5.TabIndex = 5;
+            this.lblMd5.Text = "MD5";
+            // 
+            // txtSha256
+            // 
+            this.txtSha256.Location = new System.Drawing.Point(66, 61);
+            this.txtSha256.Name = "txtSha256";
+            this.txtSha256.Size = new System.Drawing.Size(376, 20);
+            this.txtSha256.TabIndex = 8;
+            // 
+            // lblSha256
+            // 
+            this.lblSha256.AutoSize = true;
+            this.lblSha256.Location = new System.Drawing.Point(12, 61);
+            this.lblSha256.Name = "lblSha256";
+            this.lblSha256.Size = new System.Drawing.Size(47, 13);
+            this.lblSha256.TabIndex = 7;
+            this.lblSha256.Text = "SHA256";
+            // 
+            // btnCopyToClipMd5
+            // 
+            this.btnCopyToClipMd5.Location = new System.Drawing.Point(448, 35);
+            this.btnCopyToClipMd5.Name = "btnCopyToClipMd5";
+            this.btnCopyToClipMd5.Size = new System.Drawing.Size(107, 23);
+            this.btnCopyToClipMd5.TabIndex = 9;
+            this.btnCopyToClipMd5.Text = "&MD 5 To Clipboard";
+            this.btnCopyToClipMd5.UseVisualStyleBackColor = true;
+            this.btnCopyToClipMd5.Click += new System.EventHandler(this.btnCopyToClipMd5_Click);
+            // 
+            // btnCopyToClipSha
+            // 
+            this.btnCopyToClipSha.Location = new System.Drawing.Point(448, 61);
+            this.btnCopyToClipSha.Name = "btnCopyToClipSha";
+            this.btnCopyToClipSha.Size = new System.Drawing.Size(107, 23);
+            this.btnCopyToClipSha.TabIndex = 10;
+            this.btnCopyToClipSha.Text = "&SHA To Clipboard";
+            this.btnCopyToClipSha.UseVisualStyleBackColor = true;
+            this.btnCopyToClipSha.Click += new System.EventHandler(this.btnCopyToClipSha_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(16, 17);
+            this.toolStripStatusLabel1.Text = " | ";
+            // 
+            // toolStripStatusfilePath
+            // 
+            this.toolStripStatusfilePath.Name = "toolStripStatusfilePath";
+            this.toolStripStatusfilePath.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusfilePath.MouseHover += new System.EventHandler(this.StatusStrip1_MouseHover);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelVer,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusfilePath,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 99);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(564, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelVer
+            // 
+            this.toolStripStatusLabelVer.Name = "toolStripStatusLabelVer";
+            this.toolStripStatusLabelVer.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabelVer.Text = "Version";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(564, 121);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnCopyToClipSha);
+            this.Controls.Add(this.btnCopyToClipMd5);
+            this.Controls.Add(this.txtSha256);
+            this.Controls.Add(this.lblSha256);
+            this.Controls.Add(this.txtMd5);
+            this.Controls.Add(this.lblMd5);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnOpenFile);
+            this.MaximumSize = new System.Drawing.Size(580, 160);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtMd5;
+        private System.Windows.Forms.Label lblMd5;
+        private System.Windows.Forms.TextBox txtSha256;
+        private System.Windows.Forms.Label lblSha256;
+        private System.Windows.Forms.Button btnCopyToClipMd5;
+        private System.Windows.Forms.Button btnCopyToClipSha;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusfilePath;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelVer;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
     }
 }
 
--- HashGenerator/Form1.cs
+++ HashGenerator/Form1.cs
@@ -2,19 +2,154 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using  System.Security.Cryptography;
 
 namespace HashGenerator
 {
     public partial class Form1 : Form
     {
+        // The cryptographic service provider.
+        private SHA256 Sha256 = SHA256.Create();
+        private MD5 Md5 = MD5.Create();
+        private string filePath="Not Selected";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Hash Generator";
+            toolStripStatusfilePath.Text = filePath;
+            txtMd5.Enabled = false;
+            txtSha256.Enabled = false;
+            toolStripStatusLabelVer.Text = "Ver.: " + GetVersion();
+        }
+        // Compute the file's hash.
+        private byte[] GetHashSha256(string filename)
+        {
+            using (FileStream stream = File.OpenRead(filename))
+            {
+                return Sha256.ComputeHash(stream);
+            }
+        }
+
+        // Return a byte array as a sequence of hex values.
+        public static string BytesToString(byte[] bytes)
+        {
+            string result = "";
+            foreach (byte b in bytes) result += b.ToString("x2");
+            return result;
+        }
+
+        // Compute the file's hash code.
+        private void BtnHash_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private byte[] GetHashMD5(string filename)
+        {
+            using (FileStream stream = File.OpenRead(filename))
+            {
+                return Md5.ComputeHash(stream);
+            }
+        }
+
+        private void BtnOpenFile_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            //var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Zip files (*.zip)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    txtFile.Text = openFileDialog.FileName;
+                    toolStripStatusfilePath.Text = filePath;
+                    
+                    if (filePath.Length >= 100)
+                    {
+                        string[] cropPath=filePath.Split('\\');
+                        toolStripStatusfilePath.Text = cropPath[0] + "\\" +
+                                                       cropPath[1] + "\\...\\" + 
+                                                       cropPath[cropPath.Length - 1];
+                    }
+
+                    /*Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }*/
+                }
+            }
+        }
+
+        private void StatusStrip1_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.IsBalloon = true;
+            tt.InitialDelay = 0;
+            tt.ShowAlways = true;
+            tt.SetToolTip(statusStrip1, filePath);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtFile.Text != String.Empty)
+                {
+                    txtMd5.Text = BytesToString(GetHashMD5(txtFile.Text));
+                    txtSha256.Text = BytesToString(GetHashSha256(txtFile.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Please select a file to be hashed!", "Information", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCopyToClipMd5_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtMd5.Text);
+        }
+
+        private void btnCopyToClipSha_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtSha256.Text);
+        }
+
+        private string GetVersion()
+        {
+            //Version Label
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            version = version.Substring(0, version.Length - 2);
+            return version;
         }
     }
 }
